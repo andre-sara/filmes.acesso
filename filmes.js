8f74f624eb8a35975489df35b581d773296f8025
andre-sara <sarasuportetecnico@gmail.com>
2025-08-13 02:47:31 -0300

    Add files via upload

---
+++ filmes.js
@@ -0,0 +1,32 @@
+const filmes = [
+  {
+    title: "O Instituto - Série 2025",
+    image: "https://cinepop.com.br/wp-content/uploads/2025/07/o-instituto-scaled.webp",
+    enabled: true,
+    temporadas: [
+      {
+        numero: 1,
+        episodios: [
+          { nome: "Episódio 1", link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link" },
+          { nome: "Episódio 2", link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link" },
+          // Adicionar mais episódios conforme necessário
+        ]
+      }
+    ]
+  },
+  {
+    title: "Jovens Titãs",
+    image: "https://upload.wikimedia.org/wikipedia/pt/9/9e/TeenTitansLogo.JPG",
+    enabled: true,
+    temporadas: [
+      {
+        numero: 1,
+        episodios: [
+          { nome: "Episódio 1", link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link" },
+          { nome: "Episódio 2", link: "https://drive.google.com/file/d/1C8Z7Sz5JUBAcwBHQ5bciAuyFjM9LZk4C/view?usp=sharing" },
+          // Adicionar mais episódios conforme necessário
+        ]
+      }
+    ]
+  }
+];
