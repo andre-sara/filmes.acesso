0611d751aa7b9b8288e8c232a32f127390406334
andre-sara <sarasuportetecnico@gmail.com>
2025-08-13 04:10:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,56 @@ const movies = [
     enabled: true,  // habilitado
     episodes: [
       {
-        name: "Episódio 1",
+        name: "Episódio 01 - Prova Final",
         link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
       },
       {
-        name: "Episódio 2",
+        name: "Episódio 02 - Irmãs",
         link: "https://drive.google.com/file/d/1C8Z7Sz5JUBAcwBHQ5bciAuyFjM9LZk4C/view?usp=sharing",
+      },
+            {
+        name: "Episódio 03 - Separar e Conquistar",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 04 - Forças Da Natureza",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 05 - Minha Melhor Parte",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 06 - Nunca Mais",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 07 - A Troca",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 08 - No Fundo Do Mar",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 09 - Máscaras",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 10 - A Escola",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 11 - Aprendiz Parte 1",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 12 - Aprendiz Parte 2",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
+      },
+            {
+        name: "Episódio 13 - O Carro",
+        link: "https://drive.google.com/file/d/1XfSY3phJdJWiI4wcaXqxENxdeogc5rs-/view?usp=drive_link",
       },
     ],
   },
@@ -102,6 +146,60 @@ acceptBtn.onclick = () => {
   details.classList.add("hidden");
   loadLibrary();
 };
+
+
+
+
+
+function showDetails(index) {
+  library.classList.add("hidden");
+  details.classList.remove("hidden");
+
+  const movie = movies[index];
+  movieTitle.textContent = movie.title;
+  episodesList.innerHTML = "";
+
+  movie.seasons.forEach((season, seasonIndex) => {
+    // Criar botão de temporada
+    const seasonBtn = document.createElement("button");
+    seasonBtn.classList.add("season-button");
+    seasonBtn.textContent = season.title;
+    seasonBtn.onclick = () => toggleSeasonEpisodes(seasonIndex);
+
+    // Adicionar o botão de temporada ao container
+    const seasonDiv = document.createElement("div");
+    seasonDiv.classList.add("season");
+    seasonDiv.appendChild(seasonBtn);
+    episodesList.appendChild(seasonDiv);
+
+    // Criar o container de episódios (inicialmente oculto)
+    const episodeList = document.createElement("div");
+    episodeList.classList.add("episode-list");
+    season.episodes.forEach((ep) => {
+      const epDiv = document.createElement("div");
+      epDiv.classList.add("episode");
+      epDiv.innerHTML = `<a href="${ep.link}" target="_blank">${ep.name}</a>`;
+      episodeList.appendChild(epDiv);
+    });
+
+    // Adicionar os episódios ao container de temporadas
+    seasonDiv.appendChild(episodeList);
+  });
+}
+
+// Toggle para exibir ou ocultar os episódios da temporada
+function toggleSeasonEpisodes(seasonIndex) {
+  const seasonDiv = episodesList.children[seasonIndex];
+  const episodeList = seasonDiv.querySelector(".episode-list");
+
+  // Alternar a visibilidade dos episódios
+  if (episodeList.style.display === "none" || !episodeList.style.display) {
+    episodeList.style.display = "block"; // Mostrar os episódios
+  } else {
+    episodeList.style.display = "none"; // Ocultar os episódios
+  }
+}
+
 
 
 
